025d2a776b8fc10697b349e4dd7b9e10162d57c9
Wambeo <Samuelsimiyu859@gmail.com>
2022-06-10 01:16:55 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds to numbers and output the results
+ *
+ * @a: argument to be checked
+ * @b: argument to be checked
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
