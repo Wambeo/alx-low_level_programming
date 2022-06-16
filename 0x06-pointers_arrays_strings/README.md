2e5818d61fd1d9c274b59ae1f3380e35167383b5
Wambeo <Samuelsimiyu859@gmail.com>
2022-06-16 09:54:42 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Function that concatenates two strings Function that concatenates two strings, except it will use at most n bytes from src function that copies a string
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header
+ *
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_stncpy(char *dest, char *src, int n);
+#endif
