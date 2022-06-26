f3044a5e80e2783f6aaad562d7793fa125d86269
Wambeo <Samuelsimiyu859@gmail.com>
2022-06-26 05:03:40 -0700

    task 0

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc __attribute__((unused)), char* argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+int _atoi(char *s);
