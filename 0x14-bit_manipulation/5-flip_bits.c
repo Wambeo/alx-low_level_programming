1645e0a33b860fe71882d046de876561933787b4
Wambeo <Samuelsimiyu859@gmail.com>
2022-08-10 05:30:16 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,18 @@
-include "main.h"
+#include "main.h"
 
 /**
- * flip_bits - Counts the number of bits needed to be
- *             flipped to get from one number to another.
- * @n: The number.
- * @m: The number to flip n to.
+ * get_endianness - Checks the endianness.
  *
- * Return: The necessary number of bits to flip to get from n to m.
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+int get_endianness(void)
 {
-	unsigned long int xor = n ^ m, bits = 0;
+	int num = 1;
+	char *endian = (char *)&num;
 
-	while (xor > 0)
-	{
-		bits += (xor & 1);
-		xor >>= 1;
-	}
+	if (*endian == 1)
+		return (1);
 
-	return (bits);
+	return (0);
 }
